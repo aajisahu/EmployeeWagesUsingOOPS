1d218b128fd97009750d80629d1afb9c058e4107
Ashish Sahu <aajisahu@gmail.com>
2021-09-21 08:36:41 +0530

    adding switch case to solve employee wages

--- PresentAbsent.java
+++ PresentAbsent.java
@@ -6,32 +6,49 @@ import org.apache.logging.log4j.Logger;
 class Employee {
 
 	private static final Logger Log= LogManager.getLogger(PresentAbsent.class);
-	
+
+	//Constrants declaration
 	private static final int FullDay = 8;
 	private static final int WageParHour = 20;
 	private static final int empPresent = 1;
+	private static final int empPartTime=0;
+	private static final int empPartTimeHrs=4;
 
+	// CREATED CONSTRUCTOR
 	public Employee() {
-		// created constructor
 
-		int empCheck = (int)Math.floor(Math.random() * 10) % 2;
-				
-		if (empCheck == empPresent)
+		//Generating random value from 0 to 2
+		int empCheck = (int)Math.floor(Math.random() * 10) % 3;
+
+		Log.info("**** Using Switch Statements ****");
+		Log.info("");
+		
+		switch (empCheck) //if random value is 1 then employee full time
 		{
+		case empPresent:
 			int dailyWage = FullDay*WageParHour;
-			Log.info("Employee is Present");
+			Log.info("Employee is Full Time Present");
 			Log.info("Employee Daily Wage Is : "+ dailyWage);
-		}
-		else {
+			break;
+
+		case empPartTime: //if random value is 0 then employee is part time
+			int partTimeWage=empPartTimeHrs*WageParHour;
+			Log.info("Employee is Part Time Present");
+			Log.info("Employee Daily Wage Is : "+ partTimeWage);
+			break;
+
+
+		default://if random value is 2 then employee is absent
 			Log.info("Employee is Absent");
+		
 		}
+
 	}
 }
 
 public  class PresentAbsent{
 	public static void main(String[] args) {
-		new Employee(); 
-		//CREATING OBJECT OF THE CLASS SO CONSTRUCTOR AUTOMATICALLY CALLED
-
-	}
+		new  Employee();//CREATING OBJECT OF THE CLASS SO CONSTRUCTOR AUTOMATICALLY CALLED
+	}  
+	
 }
